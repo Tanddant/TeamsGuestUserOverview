a678120d1538babb219800629ff15df283c3a4ad
Dan Toft <tanddant@gmail.com>
2023-06-01 19:30:37 +0200

    🎉 - Initial commit

---
+++ src/webparts/guestUserOverview/components/IGuestUserOverviewProps.ts
@@ -0,0 +1,7 @@
+export interface IGuestUserOverviewProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
